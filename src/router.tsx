dcdfc375174fa147124caee9e39bb55c1ab0ca44
Nicolás Contreras <nicolas.contreras@connectiva.com.co>
2026-01-02 01:11:36 -0500

    feat: Add WaveVault route to router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from '@/views/Home'
+import { WaveVault } from '@/views/WaveVault'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/wavevault",
+    element: <WaveVault />
   }
 ])
 
